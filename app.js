0095bd1d948fba5d33e822321ff2b4ac17c4c619
jgbijlsma <jbijlsma.p@gmail.com>
2022-11-04 21:33:50 +0100

    add homepage

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log("Server started"));
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, World!</h1><a href='/api/book'>Get book data</a>");
+});
+
 app.get("/api/book", (req, res) => {
   res.send({
     name: "Harry Potter",
